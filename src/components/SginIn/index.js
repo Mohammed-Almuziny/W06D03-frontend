c2da3966e676124f3e92c0049b5c22cc9126f7b7
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-16 23:50:38 +0300

    added sgin in function

--- src/components/SginIn/index.js
+++ src/components/SginIn/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 export const SginIn = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const inputData = {
+      userName: event.target.userName.value,
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+    axios.post("http://localhost:5000/account/sginin", inputData)
+  };
+
   return (
     <div>
-      <h1>sgin in</h1>
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event);
+        }}
+      >
+        <input name="userName" required />
+        <input type="email" name="email" required />
+        <input type="password" name="password" required />
+        <button
+          onSubmit={(event) => {
+            handleSubmit(event);
+          }}
+        >
+          submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
