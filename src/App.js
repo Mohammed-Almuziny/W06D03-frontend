f1c3ef92c30152ab046d93a22db056b051c48c22
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-16 22:59:03 +0300

    creat header  components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 
+import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { LogIN } from "./components/LogIn";
 
 function App() {
   return (
     <div className="App">
-      <h1>APP </h1>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIN />} />
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const Home = () => {
+  const [user, setUser] = useState()
   return (
     <div>
       <h1>Home</h1>
